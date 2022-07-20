b2d7a9dabd90a37e4f13718cc9211f54acb7eeb0
Mia C <98304507+mia05c@users.noreply.github.com>
2022-07-20 00:30:17 -0700

    Add files via upload

---
+++ String Assignment/String Assignment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Please enter your name: ");
+            string yourName = Console.ReadLine();
+            Console.Write("Please enter your age: ");
+            string yourAge = Console.ReadLine();
+            Console.Write("Please enter your favorite Number: ");
+            string yourFavoriteNumber = Console.ReadLine();
+
+            yourName = yourName.ToUpper();
+
+            Console.Write(" My Name is " + yourName + ". I am " + yourAge+ " years old. And my favorite number is " +yourFavoriteNumber+ ".");
+            Console.ReadLine();
+
+            
+
+        }
+    }
+}
