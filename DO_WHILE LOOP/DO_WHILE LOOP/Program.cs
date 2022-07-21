63ef9b6b8037e1d554254e922fdeec16a196524f
Mia C <98304507+mia05c@users.noreply.github.com>
2022-07-21 00:56:16 -0700

    Add files via upload

---
+++ DO_WHILE LOOP/DO_WHILE LOOP/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DO_WHILE_LOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What year was Queen Elizabeth born?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            bool theanswer = false;
+
+            do
+            {
+                switch (age)
+                {
+                    case 1970:
+                        Console.WriteLine("You guessed 1970. Try again");
+                        Console.WriteLine("What year was Queen Elizabeth born?");
+                        age = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 1955:
+                        Console.WriteLine("You guessed 1955. Try again");
+                        Console.WriteLine("What year was Queen Elizabeth born?");
+                        age = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 1926:
+                        Console.WriteLine("Yes! She was born on April 21, 1926");
+                        theanswer = true;
+                        break;
+                    default:
+                        Console.WriteLine("Try again.");
+                        Console.WriteLine("What year was Queen Elizabeth born?");
+                        age = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+            }
+            while (!theanswer);
+            Console.Read();
+        }
+    }
+}
